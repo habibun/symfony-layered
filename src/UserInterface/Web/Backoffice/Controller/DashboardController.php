5f71df46a74bdbcd2dd79c5d1894da7bc1951bcc
Habibun Noby <habibun.noby@gmail.com>
2023-02-10 08:48:54 +0600

    fix: sf version

---
+++ src/UserInterface/Web/Backoffice/Controller/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\UserInterface\Web\Backoffice\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route(name="dashboard", path="/")
+     */
+    public function index()
+    {
+        return $this->render('@Backoffice/dashboard/index.html.twig');
+    }
+}
